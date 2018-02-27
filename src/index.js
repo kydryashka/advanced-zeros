12520d178d1fa16fb431fe61a56ff4cec6992f5c
olya <olyaionova_98@mail.ru>
2018-02-28 01:32:12 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  var n=1
+  var i=2;
+  var arr=[];
+  var map=new Map();
+
+  do{
+  if (base%i==0){
+  	map.set(i,n);
+  	base=base/i;
+  	n++;
+  } else {
+  	i++;
+  	n=1;
+  }
+} while(i<=base);
+
+  map.forEach((value,key)=>{
+  var numberOfNull=0;
+  var n=number;
+  	while (n>=key){
+  	numberOfNull+=Math.floor(n/key);
+  	n=Math.floor(n/key);	
+  }
+  arr.push(Math.floor(numberOfNull/value));
+  });
+  return Math.min.apply(null, arr);
 }
